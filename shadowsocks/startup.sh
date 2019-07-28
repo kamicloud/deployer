818dc0a7d695b32a4a8bf9ebcb383576b3f57342
Kamicloud <cy602939074@qq.com>
2019-07-28 11:21:23 +0800

    optimize

--- shadowsocks/startup.sh
+++ shadowsocks/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Start shadowsocks in foreground
-go get -u github.com/shadowsocks/go-shadowsocks2
+cd /go/bin
+go get -u -v github.com/shadowsocks/go-shadowsocks2
 go build github.com/shadowsocks/go-shadowsocks2
 go-shadowsocks2 -s ss://AES-128-CFB:$PASSWORD@:10086 -verbose
