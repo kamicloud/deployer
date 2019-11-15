796f2a8a88f8fe8cda36cd95436188265dd5e7a6
Kamicloud <cy602939074@qq.com>
2019-11-15 22:35:20 +0800

    update deployer

--- golang/startup.sh
+++ golang/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start mahjong-science-server in foreground
-cd /go/bin
+
 go get -u -v github.com/kamicloud/mahjong-science-server/...
 go build github.com/kamicloud/mahjong-science-server
-mahjong-science-server
+./mahjong-science-server
