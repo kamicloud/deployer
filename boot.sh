1b4f30381063371b39aeb80bb9f050d9467c2eb2
Kamicloud <cy602939074@qq.com>
2020-01-11 13:43:46 +0800

    spider test

--- boot.sh
+++ boot.sh
@@ -4,5 +4,5 @@ apt update -yqq
 apt upgrade -yqq
 apt install -y golang docker-compose
 git clone https://github.com/kamicloud/deployer
-cd deployer
 git clone https://github.com/kamicloud/majsoul-resources
+cd deployer
